b27faf81b4c37be97e83061bc26b9fb6e888d7ab
mennaadnan <122840643+mennaadnan@users.noreply.github.com>
2025-01-07 11:03:40 +0200

    model.py

--- model.py
+++ model.py
@@ -1,25 +1,24 @@
 from sklearn.cluster import KMeans
 
+def kmeans(df):
+    X = df
+    y = df['Customer Status']
+    
+    # Apply KMeans clustering
+    kmeans = KMeans(n_clusters=3, random_state=0)
+    kmeans.fit(X)
+    
+    # Get the cluster labels and count the number of records in each cluster
+    labels = kmeans.labels_
+    cluster_count = 1
+    text = ""
+    for label in set(labels):
+        count = len([x for x in labels if x == label])
+        text += f"Cluster {cluster_count}: {count} data points\n"
+        cluster_count += 1
+    
+    # Save the cluster counts to k.txt
+    with open("k.txt", "w") as f:
+        f.write(text)
 
-df = globals()["df"]
-
-X = df
-y = df['Customer Status']
-kmeans = KMeans(n_clusters=3, random_state=0) 
-
-kmeans.fit(X)
-
-labels = kmeans.labels_
-cluster_count = 1
-text = ""
-for label in set(labels):
-  count = len([x for x in labels if x == label])
-  text += f"Cluster {cluster_count}: {count} data points\n"
-  cluster_count+=1
-
-f = open("k.txt", "a")
-f.write(text)
-f.close()
-
-
-print("Pipeline Finished! Run final.sh to transfer data and close the container.")
+    print("Pipeline Finished! Run final.sh to transfer data and close the container.")
